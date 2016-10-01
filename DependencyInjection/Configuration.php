1824d7c3c6c2877faeeeb0e4c6fec655036d25c2
Jon Hall <jon@flowmo.co>
2016-10-01 18:46:59 +0100

    Remove unused local variables in DI classes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('jall_aop_monolog');
+        $treeBuilder->root('jall_aop_monolog');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
